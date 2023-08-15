2d5d56eea7c95fe9c2b2dee4894106a70f20a5ea
jncnn <75597882+jncnn@users.noreply.github.com>
2023-08-14 23:53:32 -0500

    numberchecker

--- Java_Practice-master/src/day1.java
+++ Java_Practice-master/src/day1.java
@@ -1,5 +1,28 @@
 public class day1 {
+
     public static void main(String[] args){
-        System.out.println();("Hello world____");
+
+        checkNumber(25);
+checkNumber(-5);
+
     }
-}
+    //Write a method called checkNumber with an int parameter number.
+    //        //
+    //        //The method should not return any value, and it needs to print out:
+    //        //
+    //        //"positive" if the parameter number is > 0
+    //        //
+    //        //"negative" if the parameter number is < 0
+    //        //
+    //        //"zero" if the parameter number is equal to 0
+    public static void checkNumber(int number){
+
+        if(number>0){
+            System.out.println("positive");
+
+        } else if(number<0){
+            System.out.println("negative");
+    }else{
+            System.out.println("zero");}
+
+}}
